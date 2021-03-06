d118150aa269cd7b08f4db37de41f0b62600c2ec
ygor-salles <ygorsallesaniceto@gmail.com>
2021-03-05 23:13:30 -0300

    Finalizando crud Users

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,29 +1,111 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup'
 
 class UserController {
-    async create(req: Request, resp: Response){
-        // Pego os somente os dados que preciso do front com destructing
+    async create(req: Request, resp: Response) {
         const { name, email } = req.body
-        
-        // Inicializo a sessão com banco de dados, e a partir dessa constante consigo fazer tudo no banco
-        const userRepository = getRepository(User)
 
-        const userAlreadyExists = await userRepository.findOne({email})
+        const schema = yup.object().shape({
+            name: yup.string().required('Nome é obrigatório'),
+            email: yup.string().email().required('Email incorreto')
+        })
+
         try {
-            //Possíveis erros
-            if(!name) throw 'Name is required'
-            if(!email) throw 'E-mail is required'
-            if(userAlreadyExists) throw 'User already exists!'
+            await schema.validate(req.body, { abortEarly: false })
+        } catch (error) {
+            return resp.status(400).json({ message: error })
+        }
 
-            //Caso não ocorra erro
-            const user = userRepository.create({name, email})
-            await userRepository.save(user)
+        const connectionUser = getCustomRepository(UsersRepository);
+
+        const userAlreadyExists = await connectionUser.findOne({ email })
+
+        if (userAlreadyExists) {
+            return resp.status(404).json({ message: 'User already exists!' })
+        }
+
+        const user = connectionUser.create({ name, email })
+        await connectionUser.save(user)
+        return resp.status(201).json(user)
+    }
+
+    async ready(req: Request, resp: Response) {
+        const connectionUser = getCustomRepository(UsersRepository)
+        const allUsers = await connectionUser.find()
+        return resp.json(allUsers)
+    }
+
+    async readyByEmail(req: Request, resp: Response) {
+        const { email } = req.body
+
+        const schema = yup.object().shape({
+            email: yup.string().email('Email is required')
+        })
+        try {
+            await schema.validate(req.body, { abortEarly: false })
+        } catch (error) {
+            return resp.status(400).json({ message: error })
+        }
+
+        const connectionUser = getCustomRepository(UsersRepository)
+        const user = await connectionUser.findOne({ email })
+        if (user) {
             return resp.json(user)
-        } catch (err) {
-            resp.status(400).json({error: err})
-        } 
+        }
+        else {
+            return resp.status(404).json({ message: 'User already exists!' })
+        }
+
+    }
+
+    async delete(req: Request, resp: Response) {
+        const { email } = req.body
+
+        const schema = yup.object().shape({
+            email: yup.string().email('Email is required')
+        })
+        try {
+            await schema.validate(req.body, { abortEarly: false })
+        } catch (error) {
+            return resp.status(400).json({ message: error })
+        }
+
+        const connectionUser = getCustomRepository(UsersRepository)
+        const user = await connectionUser.findOne({ email })
+        if (user) {
+            const status = await connectionUser.remove(user)
+            console.log(status)
+            return resp.status(200).json({ message: 'User remove success!' })
+        }
+        else {
+            return resp.status(400).json({ message: 'User already exists!' })
+        }
+    }
+
+    async update(req: Request, resp: Response) {
+        const { email } = req.body
+
+        const schema = yup.object().shape({
+            email: yup.string().email('Email is required')
+        })
+        try {
+            await schema.validate(req.body, { abortEarly: false })
+        } catch (error) {
+            return resp.status(400).json({ message: error })
+        }
+
+        const connectionUser = getCustomRepository(UsersRepository)
+        const user = await connectionUser.findOne({ email })
+        
+        if (user) {
+            const status = await connectionUser.update(user.id, req.body)
+            console.log(status)
+            return resp.status(200).json({ message: 'User update success' })
+        }
+
+        return resp.status(404).json({ message: 'User not found' })
     }
 }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,5 +6,9 @@ const router = Router()
 const userController = new UserController()
 
 router.post('/users', userController.create)
+router.get('/users', userController.ready)
+router.get('/users/:email', userController.readyByEmail)
+router.delete('/users/:email', userController.delete)
+router.put('/users/:email', userController.update)
 
 export { router }
