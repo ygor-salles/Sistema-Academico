a34824adf1cf3864bbbc6389be9ae1df40af7774
ygor-salles <ygorsallesaniceto@gmail.com>
2021-03-29 19:31:09 -0300

    separando css

--- frontend/src/components/templates/header/HeaderApp.tsx
+++ frontend/src/components/templates/header/HeaderApp.tsx
@@ -1,24 +1,10 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
+import { useStyles } from './HeaderStyles'
 
 export function HeaderApp() {
   const classes = useStyles();
@@ -31,7 +17,7 @@ export function HeaderApp() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            App academico
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
