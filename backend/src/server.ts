46ca909de10e13c900b941d993faaf06d326f377
ygor-salles <ygorsallesaniceto@gmail.com>
2021-04-02 18:18:12 -0300

    Adicionando cors no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,10 @@ import 'reflect-metadata'
 import express from 'express'
 import './database'
 import { router } from './routes'
+import cors from 'cors'
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
