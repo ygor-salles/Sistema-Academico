7fbccad175e12c198077409a79369c8e0d6c1218
ygor-salles <ygorsallesaniceto@gmail.com>
2021-04-07 23:23:02 -0300

    Capturando dados do formulário

--- frontend/src/components/discipline/DialogDiscipline.tsx
+++ frontend/src/components/discipline/DialogDiscipline.tsx
@@ -19,7 +19,16 @@ function DialogDiscipline({ selectedValue, open, onClose }: Props) {
 
     const [codeDisc, setCodeDisc] = React.useState('');
     const [nameDisc, setNameDisc] = React.useState('');
-    const [workloadDisc, setWorkloadDisc] = React.useState('');
+    const [workloadDisc, setWorkloadDisc] = React.useState(0);
+
+    function handleSubmit(){
+        const data = {
+            code: codeDisc,
+            name: nameDisc,
+            workload: workloadDisc
+        }
+        console.log(data)
+    }
 
     return (
         <>
@@ -28,19 +37,22 @@ function DialogDiscipline({ selectedValue, open, onClose }: Props) {
                 <DialogContent>
                     <form noValidate autoComplete="off">
                         <TextField className={classes.inputForm} id="code" label="Code discipline"
-                            value={codeDisc} onChange={(event) => setCodeDisc(event.target.value)} fullWidth />
+                            value={codeDisc} fullWidth
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCodeDisc(e.target.value)}  />
                         <TextField className={classes.inputForm} id="name" label="Name discipline"
-                            value={nameDisc} onChange={(event) => setNameDisc(event.target.value)} fullWidth />
+                            value={nameDisc} fullWidth
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNameDisc(e.target.value)} />
                         <TextField className={classes.inputForm} id="workload" label="Workload discipline"
-                            value={workloadDisc} onChange={(event) => setWorkloadDisc(event.target.value)} fullWidth />
+                            value={workloadDisc} fullWidth
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setWorkloadDisc(+e.target.value)} />
                     </form>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
-                        Cancel
+                        Cancelar
                 </Button>
-                    <Button onClick={handleClose} variant="contained" color="primary">
-                        Subscribe
+                    <Button onClick={handleSubmit} variant="contained" color="primary">
+                        Salvar
                 </Button>
                 </DialogActions>
             </Dialog>
