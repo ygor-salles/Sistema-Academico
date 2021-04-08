ab1678ed6ff98c57cff1f81b88b4e096a8ca42fd
ygor-salles <ygorsallesaniceto@gmail.com>
2021-04-07 22:26:57 -0300

    Formulário do modal

--- frontend/src/components/discipline/DialogDiscipline.tsx
+++ frontend/src/components/discipline/DialogDiscipline.tsx
@@ -1,9 +1,5 @@
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { Button, TextField, DialogTitle, DialogContent, DialogActions, Dialog } from '@material-ui/core';
+import React from 'react';
 import { FormDiscipline, useStyles } from './DisciplineUtils';
 
 type Props = {
@@ -17,17 +13,27 @@ function DialogDiscipline({ selectedValue, open, onClose }: Props) {
     const classes = useStyles();
 
     const handleClose = () => {
+        console.log(selectedValue);
         onClose(selectedValue);
-      };
+    };
+
+    const [codeDisc, setCodeDisc] = React.useState('');
+    const [nameDisc, setNameDisc] = React.useState('');
+    const [workloadDisc, setWorkloadDisc] = React.useState('');
 
     return (
         <>
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
                 <DialogTitle id="form-dialog-title">Cadastro de Disciplina</DialogTitle>
                 <DialogContent>
-                    <TextField className={classes.inputForm} margin="dense" id="code" label="Code discipline" fullWidth />
-                    <TextField className={classes.inputForm} margin="dense" id="name" label="Name discipline" fullWidth />
-                    <TextField className={classes.inputForm} margin="dense" id="workload" label="Workload discipline" fullWidth />
+                    <form noValidate autoComplete="off">
+                        <TextField className={classes.inputForm} id="code" label="Code discipline"
+                            value={codeDisc} onChange={(event) => setCodeDisc(event.target.value)} fullWidth />
+                        <TextField className={classes.inputForm} id="name" label="Name discipline"
+                            value={nameDisc} onChange={(event) => setNameDisc(event.target.value)} fullWidth />
+                        <TextField className={classes.inputForm} id="workload" label="Workload discipline"
+                            value={workloadDisc} onChange={(event) => setWorkloadDisc(event.target.value)} fullWidth />
+                    </form>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
--- frontend/src/components/discipline/DisciplineComponent.tsx
+++ frontend/src/components/discipline/DisciplineComponent.tsx
@@ -1,12 +1,5 @@
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import { Button, Paper, Table, TableBody, TableCell, 
+    TableContainer, TableHead, TablePagination, TableRow } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { Discipline } from '../../models/discipline.model';
 import { fetchDisciplines } from '../../services/discipline.service';
