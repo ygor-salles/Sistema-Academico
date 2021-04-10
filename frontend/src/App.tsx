6d4d166ddf065f612ecb8918c2ff87d535b3df6e
ygor-salles <ygorsallesaniceto@gmail.com>
2021-04-10 16:12:10 -0300

    Adicionando opção para mudar a aplicação para dark

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { theme, useStyles } from './AppStyles';
-import Routes  from './Routes';
+import Routes from './Routes';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 function App() {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        display: 'flex',
+      },
+    }),
+  );
   const classes = useStyles();
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+          // primary: { main: prefersDarkMode ? '#242939' : '#242939' },
+          // secondary: { main: prefersDarkMode ? '#C4C4C4' : '#C4C4C4' }
+        },
+      }),
+    [prefersDarkMode],
+  );
 
   return (
     <div className={classes.root}>
